f8e35784608ba81ac7f7b84880b63398cf637f73
WOLFANG FELIPE P�REZ ROLD�N <wolfangperez302437@correo.itm.edu.co>
2024-11-26 17:04:19 -0500

    Epatando usuario del profe con usuario nuestro.

--- Paginas/Scripts/Usuarios.js
+++ Paginas/Scripts/Usuarios.js
@@ -1,75 +1,60 @@
-﻿jQuery(function () {
-    //Registrar los botones para responder al evento click
-    $("#dvMenu").load("../Paginas/Menu.html")
+﻿class Usuario {
+    constructor(id, Documento_Empleado, userName, Clave) {
+        this.id = id;
+        this.Documento_Empleado = Documento_Empleado;
+        this.userName = userName;
+        this.Clave = Clave;
+    }
+}
+jQuery(function () {
+    //Se ejecuta al cargar la página
+    LlenarComboXServicios("https://localhost:44323/api/Perfiles/LlenarCombo", "#cboPerfil");
+    LlenarTabla();
 });
-async function Consultar() {
-    //alert("Click en el botón consultar");
-    //console.log("Click en el botón consultar");
-    /*
-        let: Variable de ambito local, que solo aplica a la función
-        var: Variable de ambito global, que aplica para la página que se está trabajando
-        const: Constante. Se utiliza para la definición de objetos
-    */
-    let Documento = $("#txtDocumento").val();
-    //Para invocar el servicio, vamos a utilizar el método fetch de javascript, que permite un servicio en una url con unos parámetros definidos
-    try {
-        const Resultado = await fetch("https://localhost:44323/apli/Clientes/ConsultarXDocumento?Documento=" + Documento,
-            {
-                method: "GET",
-                mode: "cors",
-                headers: { "Content-Type": "application/json"}
-            });
-        //Se transforma el Resultado a un formato Json para procesarlo en el html
-        const Respuesta = await Resultado.json();
-        $("#txtNombre").val(Respuesta.Nombre);
-        $("#txtPrimerApellido").val(Respuesta.PrimerApellido);
-        $("#txtSegundoApellido").val(Respuesta.SegundoApellido);
-        $("#txtEmail").val(Respuesta.Email);
-        $("#txtDireccion").val(Respuesta.Direccion);
-        $("#txtFechaNacimiento").val(Respuesta.FechaNacimiento.split('T')[0]);
-    }
-    catch (error) {
-        $("#dvMensaje").html(error);
-    }
+function Editar(Documento, Empleado, Cargo, Usuario, idPerfil, Activo, idUsuarioPerfil) {
+    $("#txtDocumento").val(Documento);
+    $("#txtNombre").val(Empleado);
+    $("#txtCargo").val(Cargo);
+    $("#txtUsuario").val(Usuario);
+    $("#cboPerfil").val(idPerfil);
+    $("#txtidUsuarioPerfil").val(idUsuarioPerfil);
+    $("#txtActivo").val(Activo == "True" ? "SI" : "NO");
 }
-function Insertar() {
-    EjecutarComando("POST", "Insertar");
+function LlenarTabla() {
+    LlenarTablaXServicios("https://localhost:44323/api/Usuarios/ListarUsuarios", "#tblUsuarios");
 }
-function Actualizar() {
-    EjecutarComando("PUT", "Actualizar");
-}
-function Eliminar() {
-    EjecutarComando("DELETE", "Eliminar");
-}
-
 async function EjecutarComando(Metodo, Funcion) {
-    const cliente = new Cliente($("#txtDocumento").val(), $("#txtNombre").val(), $("#txtPrimerApellido").val(), $("#txtSegundoApellido").val(),
-        $("#txtEmail").val(), $("#txtDireccion").val(), $("#txtFechaNacimiento").val());
-
-    try {
-        const Resultado = await fetch("https://localhost:44323/apli/Clientes/" + Funcion,
-            {
-                method: Metodo,
-                mode: "cors",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(cliente)
-            });
-        const Respuesta = await Resultado.json();
-        $("#dvMensaje").html(Respuesta);
+    let idPerfil = $("#cboPerfil").val();
+    let Clave = $("#txtClave").val();
+    let RepitaClave = $("#txtConfirmaClave").val();
+    if (Clave != RepitaClave) {
+        $("#dvMensaje").html("Las claves no son iguales");
+        return;
     }
-    catch (error) {
-        $("#dvMensaje").html(error);
-    }
+    let URL = "https://localhost:44323/api/Usuarios/" + Funcion + "?Perfil=" + idPerfil;
+    const usuario = new Usuario(0, $("#txtDocumento").val(), $("#txtUsuario").val(), Clave);
+    await EjecutarServicio(Metodo, URL, usuario);
+    LlenarTabla();
 }
-//Clases en javascript
-class Cliente {
-    constructor(Documento, Nombre, PrimerApellido, SegundoApellido, Email, Direccion, FechaNacimiento) {
-        this.Documento = Documento;
-        this.Nombre = Nombre;
-        this.PrimerApellido = PrimerApellido;
-        this.SegundoApellido = SegundoApellido;
-        this.Email = Email;
-        this.Direccion = Direccion;
-        this.FechaNacimiento = FechaNacimiento;
+async function Activar(Activo) {
+    let idUsuarioPerfil = $("#txtidUsuarioPerfil").val();
+    let URL = "https://localhost:44323/api/Usuarios/Activar?idUsuarioPerfil=" + idUsuarioPerfil + "&Activo=" + Activo;
+    const usuario = new Usuario(0, $("#txtDocumento").val(), $("#txtUsuario").val(), "");
+    await EjecutarServicio("PUT", URL, usuario);
+    LlenarTabla();
+}
+async function BuscarEmpleado() {
+    let Documento = $("#txtDocumento").val();
+    let URL = "https://localhost:44323/api/Empleados/ConsultarXDocumento?Documento=" + Documento;
+    const empleado = await ConsultarServicio(URL);
+    if (empleado == null) {
+        $("#dvMensaje").html("El documento del empleado no existe en la base de datos, o no tiene información completa");
+        $("#txtNombre").val("");
+        $("#txtCargo").val("");
+    }
+    else {
+        $("#dvMensaje").html("");
+        $("#txtNombre").val(empleado[0].Empleado);
+        $("#txtCargo").val(empleado[0].Cargo);
     }
 }
