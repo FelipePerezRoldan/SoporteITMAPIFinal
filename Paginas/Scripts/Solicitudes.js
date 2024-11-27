851a055396cb822d30e9b876a084740f519ef82b
WOLFANG FELIPE P�REZ ROLD�N <wolfangperez302437@correo.itm.edu.co>
2024-11-26 20:10:27 -0500

    Listo el front de solicitud.

--- Paginas/Scripts/Solicitudes.js
+++ Paginas/Scripts/Solicitudes.js
@@ -1,48 +1,61 @@
-﻿jQuery(function () {
-    //Se ejecuta al cargar la página
-    LlenarComboXServiciosAuth("https://localhost:44323/api/TipoProductos/ListarTodos", "#cboTipoProducto");
-
-    LlenarTabla();
-});
-function LlenarTabla() {
-    LlenarTablaXServiciosAuth("https://localhost:44323/api/Productos/ListarProductosConTipo", "#tblProductos");
-}
-class Producto {
-    constructor(Codigo, Nombre, Descripcion, Cantidad, ValorUnitario, CodigoTipoProducto) {
-        this.Codigo = Codigo;
-        this.Nombre = Nombre;
+﻿class Solicitud {
+    constructor(NumeroSolicitud, Tipo, Titulo, Descripcion, FechaCreación, FechaResolucion, UsuarioSolicitante,UsuarioAsignado,Estado, Categoria) {
+        this.NumeroSolicitud = NumeroSolicitud;
+        this.Tipo = Tipo;
+        this.Titulo = Titulo;
         this.Descripcion = Descripcion;
-        this.Cantidad = Cantidad;
-        this.ValorUnitario = ValorUnitario;
-        this.CodigoTipoProducto = CodigoTipoProducto;
+        this.FechaCreación = FechaCreación;
+        this.FechaResolucion = FechaResolucion;
+        this.UsuarioSolicitante = UsuarioSolicitante;
+        this.UsuarioAsignado = UsuarioAsignado;
+        this.Estado = Estado;
+        this.Categoria = Categoria;
     }
 }
-async function EjecutarComando(Metodo, Funcion) {
-    let URL = "https://localhost:44323/api/Productos/" + Funcion;
-    const producto = new Producto($("#txtCodigo").val(), $("#txtNombre").val(), $("#txtDescripcion").val(),
-        $("#txtCantidad").val(), $("#txtValorUnitario").val(), $("#cboTipoProducto").val());
-    await EjecutarServicioAuth(Metodo, URL, producto);
+jQuery(function ()
+{
+    //Se ejecuta al cargar la página
+    LlenarComboXServiciosAuth("https://localhost:44391/api/TipoSolicitudes/ListarTodos", "#cboTipoSolicitud");
+    LlenarTabla();
+});
+function LlenarTabla()
+{
+    LlenarTablaXServiciosAuth("https://localhost:44391/api/Solicitudes/ListarSolicitudesConTipo", "#tblSolicitudes");
+}
+async function EjecutarComando(Metodo, Funcion)
+{
+    let URL = "https://localhost:44391//api/Solicitudes/" + Funcion;
+    const solicitud = new Solicitud($("#txtSolicitud").val(), $("#txtTipo").val(), $("#txtDescripcion").val(),
+    $("#txtFechaCreacion").val(), $("#txtFechaResolucion").val(), $("#txtSolicitante").val(), $("#txtAsignado").val(),
+    $("#txtEstado").val(), $("#txtCategoria").val());
+    await EjecutarServicioAuth(Metodo, URL, Solicitud);
     LlenarTabla();
 }
 async function Consultar() {
-    let Codigo = $("#txtCodigo").val();
-    let URL = "https://localhost:44323/api/Productos/Consultar?Codigo=" + Codigo;
-    const producto = await ConsultarServicioAuth(URL);
-    if (producto == null) {
-        $("#dvMensaje").html("El código del producto no existe en la base de datos");
-        $("#txtNombre").val("");
+    let Codigo = $("#txtSolicitud").val();
+    let URL = "https://localhost:44391//api/Solicitudes/Consultar?Codigo=" + Codigo;
+    const solicitud = await ConsultarServicioAuth(URL);
+    if (solicitud == null) {
+        $("#dvMensaje").html("El código del Solicitud no existe en la base de datos");
+        $("#txtTipo").val("");
         $("#txtDescripcion").val("");
-        $("#txtCantidad").val("");
-        $("#txtValorUnitario").val("");
-        $("#cboTipoProducto").val("");
+        $("#txtFechaCreacion").val("");
+        $("#txtFechaResolucion").val("");
+        $("#txtSolicitante").val("");
+        $("#txtAsignado").val("");
+        $("#txtEstado").val("");
+        $("#txtCategoria").val("");
     }
     else {
         //Escribir las respuestas
-        $("#txtNombre").val(producto.Nombre);
-        $("#txtDescripcion").val(producto.Descripcion);
-        $("#txtCantidad").val(producto.Cantidad);
-        $("#txtValorUnitario").val(producto.ValorUnitario);
-        $("#cboTipoProducto").val(producto.CodigoTipoProducto);
+        $("#txtTipo").val(solicitud.Tipo);
+        $("#txtDescripcion").val(solicitud.Descripcion);
+        $("#txtFechaCreacion").val(solicitud.FechaCreación);
+        $("#txtFechaResolucion").val(solicitud.FechaResolucion);
+        $("#txtSolicitante").val(solicitud.UsuarioSolicitante);
+        $("#txtAsignado").val(solicitud.UsuarioAsignado);
+        $("#txtEstado").val(solicitud.Estado);
+        $("#txtCategoria").val(solicitud.Categoria);
         $("#dvMensaje").html("");
     }
 }
